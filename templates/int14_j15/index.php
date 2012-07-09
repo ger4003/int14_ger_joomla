0f37d66c1abc269a0b724c37e4dc3e411b84c90d
ger4003 <ger4003@googlemail.com>
2012-07-09 15:00:00 +0200

    add google analytics code to index.php, update thumb images for category view

--- templates/int14_j15/index.php
+++ templates/int14_j15/index.php
@@ -59,6 +59,21 @@ if(JRequest::getCmd('view') != "frontpage")
 
 	<meta property="og:image" content="<?php echo $templatePath?>/images/fb_share.jpg" />
 
+	<!-- google analytics -->
+	<script type="text/javascript">
+
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-31956241-1']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+
+	</script>
+
 </head>
 
 <body>
